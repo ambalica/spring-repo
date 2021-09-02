8c7c2f1dc145eeab97ac0159dee7f89f2bf167ac
Ambalica Angra <amby91@gmail.com>
2021-09-03 01:36:19 +0530

    Update SpringrestExternalAPI.java
    Updating the project with new name

--- springrest_with_externalAPI/src/main/java/com/springrest/springrest/SpringrestExternalAPI.java
+++ springrest_with_externalAPI/src/main/java/com/springrest/springrest/SpringrestExternalAPI.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
-public class SpringrestApplication {
+public class SpringrestExternalAPI {
 	
 	@Bean
 	public RestTemplate getRestTemplate(){
 		return new RestTemplate();		
 	}
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringrestExternalAPI.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringrestApplication.class, args);
+		SpringApplication.run(SpringrestExternalAPI.class, args);
 	}
 
 }
